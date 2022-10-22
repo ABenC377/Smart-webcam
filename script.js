a5bbe3983e8ee536bdc81b7d56ce2d99bedf941a
ABenC377 <84676155+ABenC377@users.noreply.github.com>
2022-10-22 14:09:58 +0100

    Adding some JS logic

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
+const video = document.getElementById('webcam');
+const liveView = document.getElementById('liveView');
+const demosSection = document.getElementById('demos');
+const enableWebcamButton = document.getElementById('camEnableButton');
+
 const status = document.getElementById('status');
 if (status) {
     status.innerText = 'Loaded TensorFlow.js - version: ' + tf.version.tfjs;
 }
+
+function getUserMediaSupported() {
+    return !!(navigator.mediaDevices &&
+    navigator.mediaDevices.getUserMedia);
+}
+
+if (getUserMediaSupported()) {
+    enableWebcamButton.addEventListener('click', enableCam);
+} else {
+    console.warn('getUserMedia() is not supported by your browser');
+}
+
+function enableCam(event) {
+    if (!model) {
+        return;
+    }
+
+    event.target.classList.add('removed');
+
+    const constraints = {
+        video: true
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+        video.srcObject = stream;
+        video.addEventListener('loadeddata', predictWebcam);
+    });
+}
+
+function predictWebcam() {
+}
+
+var model = true;
+demosSection.classList.remove('invisible');
